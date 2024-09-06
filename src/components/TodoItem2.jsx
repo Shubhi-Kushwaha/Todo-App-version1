8bd10d806c68c2a946a1839e0ceec0946f17e2e1
Shubhi-Kushwaha <kushwahashubhi25@gmail.com>
2024-09-06 10:25:40 -0700

    first change

---
+++ src/components/TodoItem2.jsx
@@ -0,0 +1,19 @@
+function TodoItem2(){
+  let todoName=" Go to College";
+  let todoDate=' 06/09/24';
+
+   return  <div class="container">
+      <div class="row kg-row">
+          <div class="col-4">
+          {todoName}
+          </div>
+          <div class="col-4">
+          {todoDate}
+          </div>
+          <div class="col-2">
+          <button type="button" class="btn btn-danger kg-button">Delete</button>
+          </div>
+        </div>
+   </div>
+}
+export default TodoItem2;
